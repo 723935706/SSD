3523303aefab824b6888de6fa29b6d26c8bb1e4c
723935706 <48706738+723935706@users.noreply.github.com>
2020-08-15 16:34:26 +0800

    Update shufflenetv2.py

--- ssd/modeling/backbone/shufflenetv2.py
+++ ssd/modeling/backbone/shufflenetv2.py
@@ -191,7 +191,7 @@ def _shufflenetv2(arch, pretrained, *args, **kwargs):
             raise NotImplementedError('pretrained {} is not supported as of now'.format(arch))
         else:
             state_dict = load_state_dict_from_url(model_url)
-            model.load_state_dict(state_dict，strict = False)
+            model.load_state_dict(state_dict, strict=False)
 
     return model
 
