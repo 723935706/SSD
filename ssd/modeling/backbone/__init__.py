6b84782deae87f31dbf27a513abfdc3336fa392b
723935706 <48706738+723935706@users.noreply.github.com>
2020-08-19 23:51:05 +0800

    delete cbam

--- ssd/modeling/backbone/__init__.py
+++ ssd/modeling/backbone/__init__.py
@@ -3,9 +3,8 @@ from .vgg import VGG
 from .mobilenet import MobileNetV2
 from .efficient_net import EfficientNet
 from .shufflenetv2 import ShuffleNetV2
-from .shufflenetv2_cbam import ShuffleNetV2_cbam
 
-__all__ = ['build_backbone', 'VGG', 'MobileNetV2', 'EfficientNet', 'ShuffleNetV2', 'ShuffleNetV2_cbam']
+__all__ = ['build_backbone', 'VGG', 'MobileNetV2', 'EfficientNet', 'ShuffleNetV2']
 
 
 def build_backbone(cfg):
