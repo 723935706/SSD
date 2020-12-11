a430bf2d993b03f347a6923a071f53984f1a372c
723935706 <48706738+723935706@users.noreply.github.com>
2020-12-11 13:58:05 +0800

    Update __init__.py

--- ssd/modeling/backbone/__init__.py
+++ ssd/modeling/backbone/__init__.py
@@ -3,8 +3,9 @@ from .vgg import VGG
 from .mobilenet import MobileNetV2
 from .efficient_net import EfficientNet
 from .shufflenetv2 import ShuffleNetV2
+from .childnet import ChildNet
 
-__all__ = ['build_backbone', 'VGG', 'MobileNetV2', 'EfficientNet', 'ShuffleNetV2']
+__all__ = ['build_backbone', 'VGG', 'MobileNetV2', 'EfficientNet', 'ShuffleNetV2', 'ChildNet']
 
 
 def build_backbone(cfg):
